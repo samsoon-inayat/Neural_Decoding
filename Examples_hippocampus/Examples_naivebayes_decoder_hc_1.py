959b27ee7e2d0c628c01c3aa87163603fa4d7691
Samsoon Inayat <samsoon.inayat@gmail.com>
2021-04-14 22:03:19 -0600

    -

--- Examples_hippocampus/Examples_naivebayes_decoder_hc_1.py
+++ Examples_hippocampus/Examples_naivebayes_decoder_hc_1.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Example of Naive Bayes decoder
-# 
-# This example is similar to those shown in "Examples_all_decoders."
-# However, there is some preprocessing that is different for the Naive Bayes decoder, so we have made a separate notebook.
-# 
-# In this example notebook, we:
-# 1. Import the necessary packages
-# 2. Load a data file (spike trains and outputs we are predicting)
-# 3. Preprocess the data
-# 4. Run the decoders and print the goodness of fit
-# 5. Plot example decoded outputs
-
-# ## 1. Import Packages
-# 
-# Below, we import both standard packages, and functions from the accompanying .py files
-# Note that you may need to specify the path below
-
-# In[94]:
-
 
 #Import standard packages
 import numpy as np
@@ -43,34 +21,9 @@ from Neural_Decoding.metrics import get_rho
 from Neural_Decoding.decoders import NaiveBayesDecoder
 
 
-# ## 2. Load Data
-# The data for this example can be downloaded at this [link](https://www.dropbox.com/sh/n4924ipcfjqc0t6/AACPWjxDKPEzQiXKUUFriFkJa?dl=0&preview=example_data_hc.pickle). 
-# 
-# The data that we load is in the format described below. We have another example script, "neural_preprocessing.py" that may be helpful towards putting the data in this format.
-# 
-# Neural data should be a matrix of size "number of time bins" x "number of neurons", where each entry is the firing rate of a given neuron in a given time bin
-# 
-# The output you are decoding should be a matrix of size "number of time bins" x "number of features you are decoding"
-
-# In[112]:
-
 
 folder1='E:/Users/samsoon.inayat/OneDrive - University of Lethbridge/Data/Neural_Decoding/' #ENTER THE FOLDER THAT YOUR DATA IS IN
 folder = 'Z:/homes/brendan.mcallister/2P/ProcessedDataMatlab/'
-# folder='G:/OneDrives/OneDrive - University of Lethbridge/Data/Neural_Decoding/' #ENTER THE FOLDER THAT YOUR DATA IS IN
-
-# folder='/home/jglaser/Data/DecData/' 
-# folder='/Users/jig289/Dropbox/Public/Decoding_Data/'
-
-
-with open(folder1+'example_data_hc.pickle','rb') as f:
-    neural_data,pos_binned=pickle.load(f,encoding='latin1') #If using python 3
-# #     neural_data,pos_binned=pickle.load(f)
-    
-# # #If you would prefer to load the '.h5' example file rather than the '.pickle' example file.
-# # data=dd.io.load(folder+'example_data_hc.h5')
-# # neural_data=data['neural_data']
-# # pos_binned=data['pos_binned']
 
 filename = folder + 'NB_decoding_C_Place.mat'
 arrays = {}
@@ -85,7 +38,7 @@ for k, v in fm.items():
 # In[113]:
 
 
-an = 1
+an = 2
 cn = 0
 # num_points = 20000
 aXs_C = fm[fm['mean_rasters_T'][cn][an]]
@@ -102,37 +55,15 @@ aYs_C1[:,0] = aYs_C1p
 aYs_C1[:,1] = aYs_C1p
 plt.figure(figsize=(8, 4))
 plt.plot(aYs_C1p,aXs_C1[:,1])
-# plt.xlim([0,10000])
-# plt.figure(figsize=(8, 4))
-# plt.plot(neural_data[:,0])
-# plt.xlim([0,10000])
 neural_data = aXs_C1
 pos_binned = aYs_C1
-# fm.close()
 
 plt.imshow(neural_data)
 
-
-# ## 3. Preprocess Data
-
-# ### 3A. User Inputs
-# The user can define what time period to use spikes from (with respect to the output).
-
-# In[114]:
 bins_before=0 #How many bins of neural data prior to the output are used for decoding
 bins_current=1 #Whether to use concurrent time bin of neural data
 bins_after=0 #How many bins of neural data after the output are used for decoding
 
-# bins_before=4 #How many bins of neural data prior to the output are used for decoding
-# bins_current=1 #Whether to use concurrent time bin of neural data
-# bins_after=5 #How many bins of neural data after the output are used for decoding
-
-
-# ### 3B. Format Covariates
-
-# #### Format Input
-
-# In[115]:
 
 
 #Remove neurons with too few spikes in HC dataset
@@ -142,26 +73,12 @@ neural_data=np.delete(neural_data,rmv_nrn,1) #Remove those neurons
 X=neural_data
 
 
-# #### Format Output
-
-# In[116]:
-
-
 #Set decoding output
 y=pos_binned
 
 
-# ### 3C. More formatting
-
-# In[117]:
-
-
 #Number of bins to sum spikes over
 N=bins_before+bins_current+bins_after 
-
-
-# In[118]:
-
 
 #Remove time bins with no output (y value)
 rmv_time=np.where(np.isnan(y[:,0]))# | np.isnan(y[:,1]))
@@ -169,25 +86,11 @@ X=np.delete(X,rmv_time,0)
 y=np.delete(y,rmv_time,0)
 
 
-# ### 3D. Split into training/testing/validation sets
-# Note that parameters should be setting using a separate validation set. 
-# Then, the goodness of fit should be be tested on a testing set (separate from the training and validation sets).
-
-# ### User Options
-
-# In[119]:
-
-
 #Set what part of data should be part of the training/testing/validation sets
 
 training_range=[0, 0.5]
 valid_range=[0.5,0.65]
 testing_range=[0.65, 0.8]
-
-
-# ### Split Data: For Naive Bayes
-
-# In[120]:
 
 
 #Number of examples after taking into account bins removed for lag alignment
@@ -210,14 +113,6 @@ y_test=y[testing_set]
 #Get validation data
 X_valid=X[valid_set,:]
 y_valid=y[valid_set]
-
-
-# ### Combine data across specified bins
-# Get total number of spikes across "bins_before","bins_current",and "bins_after"
-# 
-# 
-
-# In[121]:
 
 
 #Initialize matrices for neural data in Naive bayes format
@@ -253,12 +148,6 @@ if bins_before>0 and bins_after==0:
     y_test=y_test[bins_before:]
 
 
-# ## 4. Run Decoder
-# Note that in this example, we are evaluating the model fit on the validation set
-
-# In[122]:
-
-
 #Declare model
 
 #The parameter "encoding_model" can either be linear or quadratic, although additional encoding models could later be added.
@@ -275,55 +164,21 @@ model_nb=NaiveBayesDecoder(encoding_model='quadratic')
 model_nb.fit(X_b_train,y_train)
 
 
-# In[123]:
-
-
 #Get predictions
 y_train_predicted=model_nb.predict(X_b_train,y_train)
-
-
-# In[ ]:
-
 
 #Get predictions
 y_valid_predicted=model_nb.predict(X_b_valid,y_valid)
 
 
-# In[ ]:
-
-
 #Get metric of fit
 R2_nb=get_R2(y_valid,y_valid_predicted)
 print(R2_nb)
 
-
-# In[ ]:
-
-
-#Make example plot
-# plt.plot(y_valid[2000:2500,1])
-# plt.plot(y_valid_predicted[2000:2500,1])
+fig = plt.figure()
 plt.plot(y_valid,'b')
 plt.plot(y_valid_predicted,'r')
 
-
-# In[ ]:
-
-
-#Get metric of fit
-R2_nb=get_R2(y_valid,y_valid_predicted)
-print(R2_nb)
-
-
-# In[ ]:
-
-
-#Make example plot
-plt.plot(y_valid[2000:2500,1])
-plt.plot(y_valid_predicted[2000:2500,1])
-
-
-# In[ ]:
 
 
 
